ce74e689361412eb228542eb7ab270c5f10994fd
abelraju <abelraju12@gmail.com>
2023-08-13 20:55:53 +0530

    Developing a chatbot:

--- chatbot/train.py
+++ chatbot/train.py
@@ -6,6 +6,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+from model import NeuralNet
+
 with open('intents.json', 'r') as f:
     intents = json.load(f)
     
@@ -45,22 +47,76 @@ for (pattern, tag) in xy:
 x_train = np.array(x_train)
 y_train = np.array(y_train)
 
+# Hyperparameters
+batch_size = 8
+input_size = len(x_train[0])
+hidden_size = 8
+output_size = len(tags)
+learning_rate = 0.001
+num_epochs = 1000
+
 class ChatDataset(Dataset):
+
     def __init__(self):
         self.n_samples = len(x_train)
         self.x_data = x_train
         self.y_data = y_train
-        
-    #dataset[index]
+
+    # support indexing such that dataset[i] can be used to get i-th sample
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
-    
+
+    # we can call len(dataset) to return the size
     def __len__(self):
         return self.n_samples
-    
-# Hyperparameters
-batch_size = 8
 
 dataset = ChatDataset()
-train_loader = DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True, num_workers = 2)
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=0)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train the model
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
         
+        # Forward pass
+        outputs = model(words)
+        # if y would be one-hot, we must apply
+        # labels = torch.max(labels, 1)[1]
+        loss = criterion(outputs, labels)
+        
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    if (epoch+1) % 100 == 0:
+        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+
+print(f'final loss: {loss.item():.4f}')
+
+data = {
+"model_state": model.state_dict(),
+"input_size": input_size,
+"hidden_size": hidden_size,
+"output_size": output_size,
+"all_words": all_words,
+"tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'training complete. file saved to {FILE}')

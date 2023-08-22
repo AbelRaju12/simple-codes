7ee0538babeb6fd66a6fadbc7a0bb67889ec8a93
abelraju <abelraju12@gmail.com>
2023-08-22 20:56:21 +0530

    adding html pages, working with the page content, adding bootstrap, importing pages, using varible to refer from py file..

--- Flasking/flaskblog.py
+++ Flasking/flaskblog.py
@@ -1,14 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts =[
+    {
+        'author': ' Gouri Varma',
+        'title': 'My cooking recipes',
+        'content': 'Blog content',
+        'date_posted': 'April 29, 2023'                
+    },
+    {
+        'author': ' Abel Raju',
+        'title': 'My gaming life',
+        'content': 'blog content',
+        'date_posted': 'April 30, 2023'                
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def Home():
-    return "<h1> Home  Page </h1>"
+    return render_template('home.html', posts = posts) #first posts is a variable
+    # '''
+    # <!doctype html>
+    # <html>
+    # </html>
+    # '''
 
 @app.route("/about")
 def about():
-    return "<h2>About page</h2>"
+    return render_template('about.html', title = 'About page')
 
 
 if __name__ == '__main__': #we can run app directly instead of flask run
--- Flasking/readme.md
+++ Flasking/readme.md
@@ -12,3 +12,16 @@ flask run
 if __name__ == '__main__':
     app.run(debug=True)
 #we can run app directly instead of flask run
+
+return render_template('about.html', title = 'About page')
+we can add variables like 'title' and call it in html like 
+    {% if title %}
+        <title> Flaskblog - {{ title }} </title>
+    {% else %}
+        <title> Flaskblog </title>
+    {% endif %}
+
+we can import contents of an html using {% extends name.html %}
+
+{% block content %}{% endblock %} 
+Here 'content' is user defined

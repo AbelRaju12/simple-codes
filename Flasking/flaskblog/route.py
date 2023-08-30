2f632199766eace4f3f0cb0ed17361015fcc7832
abelraju <abelraju12@gmail.com>
2023-08-30 21:23:39 +0530

    Woking with database, adding logout. quality of life

--- Flasking/flaskblog/route.py
+++ Flasking/flaskblog/route.py
@@ -1,47 +1,72 @@
-from flask import render_template, url_for, flash, redirect
-from flaskblog import app
-from flaskblog.forms import Registrationform, Loginform
+from flask import render_template, url_for, flash, redirect, request
+from flaskblog import app, db, bcrypt
+from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.model import User, Post
+from flask_login import login_required, login_user, current_user, logout_user
 
-posts =[
+posts = [
     {
-        'author': ' Gouri Varma',
-        'title': 'My cooking recipes',
-        'content': 'Blog content',
-        'date_posted': 'April 29, 2023'                
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
     },
     {
-        'author': ' Abel Raju',
-        'title': 'My gaming life',
-        'content': 'blog content',
-        'date_posted': 'April 30, 2023'                
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
     }
 ]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts = posts) #first posts is a variable
-  
+    return render_template('home.html', posts=posts)
+
+
 @app.route("/about")
 def about():
-    return render_template('about.html', title = 'About page')
+    return render_template('about.html', title='About')
 
-@app.route('/register', methods = ['GET', 'POST'])
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    form = Registrationform()
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for("home"))
-    return render_template('register.html', title = 'Register', form = form)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
-@app.route('/login', methods = ['GET', 'POST'])
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = Loginform()
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('Login unsuccessful', 'danger')
-    return render_template('login.html', title = 'Login', form = form)
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title = 'Account')
